6b35e85b01c167f6132035ccabb02746d9e9e8a4
reas032 <reireas23@gmail.com>
2021-05-26 05:55:31 +0800

    adding color and border in confirm password

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -3,9 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+        <div class="col-xl-10">
+            <div class="card border-dark rounded">
+                <div class="card-header h2 bg-info">
+                    Confirm Password
+                </div>
 
                 <div class="card-body">
                     {{ __('Please confirm your password before continuing.') }}
