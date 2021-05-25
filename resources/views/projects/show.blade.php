28e9028a02b4dba12eb7551d7245000466a31cac
reas032 <reireas23@gmail.com>
2021-05-26 07:08:03 +0800

    add h5 in show blade

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -29,7 +29,7 @@
         </div>
     </div>
 
-    <div class="row">
+    <div class="row h5">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Title:</strong>
