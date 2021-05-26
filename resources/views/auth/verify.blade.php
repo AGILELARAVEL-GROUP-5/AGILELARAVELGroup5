886b1b6698b13558c24f07caaf5f521769550b84
reas032 <reireas23@gmail.com>
2021-05-26 14:25:15 +0800

    h5 bold width

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -9,7 +9,7 @@
                     Verify Your Email Address
                 </div>
 
-                <div class="card-body">
+                <div class="card-body h5 font-weight-bold">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
@@ -20,7 +20,7 @@
                     {{ __('If you did not receive the email') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" style ="width:100%;" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
                     </form>
                 </div>
             </div>
