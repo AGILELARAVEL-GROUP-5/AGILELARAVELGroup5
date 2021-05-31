920aecc0d81a5ddccf8ce4fd61476ad755f08df8
reas032 <reireas23@gmail.com>
2021-05-31 11:25:51 +0800

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('content')
@@ -9,45 +8,83 @@
             <div class="card border border-dark border-5">
                 <div class="card-header h1 font-weight-bolder bg-info">
 
-                    Posts
+                    Post CRUD
 
                 </div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                
-                    <div class="row">
+                <br>
+
+                <div class="text-center">
+                        <img src="{{asset('/images/agilelogo.png')}}"  class="img-fluid"  >
+                </div>
+
+                <br>
+                <br>  
+
+
+                <div class="row justify-content-lg-center">          
+                        <div class="col-lg-9">    
+
+
+                <div class="row">
                         <div class="col-lg-12 margin-tb">
-                            <div class="float-left">
+
+                        <div class="float-left">
                                 <a class="btn btn-link" href="{{ route('posts.index') }}"> 
                                     All Posts
                                 </a>
+                        </div>
+                            
+                            <div class="float-right">
+                                <a class="btn btn-success" href="{{ route('posts.create') }}"> 
+                                 Create a Post
+                                </a>
                             </div>
-                        
+                        </div>
+                </div>
+             
+                <br>
+                <br>
+                         
+
+                <div class="content">
+
+                <a href="{{ route('posts.show', [$post->slug]) }}">
+                         <h1 class="title">{{ $post->title }}</h1>
+                </a>
+                    <p><b>Posted:</b> {{ $post->created_at->diffForHumans() }}</p>
+                    <p><b>Category:</b> {{ $post->category }}</p>
+                    <p><b>Content:</b> {{ $post->content }}</p>
+
+                <form method="post" action="{{ route('posts.destroy', $post) }}">
+                
+                @csrf
+                @method('delete')
+                    <div class="field is-grouped">
+                        <div class="control">
+                            <a href="{{ route('posts.edit', $post) }}" class="button is-info is-outlined">
+                                Edit
+                            </a>
+                        </div>
+
+                        <div class="control">
+                            <button type="submit" class="button is-danger is-outlined">Delete</button>
                         </div>
                     </div>
+                </form>
 
-                    @if (session('notification'))
-                        <div class="notification is-primary">
-                            {{ session('notification') }}
-                        </div>
-                    @endif
-                    <div class="content">
-
-
-        <h1 class="title">{{ $post->title }}</h1>
- 
-    <p><b>Posted:</b> {{ $post->created_at->diffForHumans() }}</p>
-    <p><b>Category:</b> {{ $post->category }}</p>
-    <p><b>Content:</b>  {{ $post->content }}</p>
+        
+            </div>
+        </div>
+    </div>
 
 
-</div>
+                <br>
+                <br>
+                <br>
+
 
 
                 </div>
