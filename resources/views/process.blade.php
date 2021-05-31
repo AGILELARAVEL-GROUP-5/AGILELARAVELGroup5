2b31d3a64c72e5d6a1d953bfc5e3da53816c16eb
reas032 <reireas23@gmail.com>
2021-05-31 11:27:57 +0800

    Update process.blade.php

--- resources/views/process.blade.php
+++ resources/views/process.blade.php
@@ -12,11 +12,7 @@
                 </div>
                 
                 <div class="card-body h5">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                  
 
                     
       <br>
